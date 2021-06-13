191280ac0603605f4e2606acbae7d78774fccde1
choibyunghyeon <choi8608@gmail.com>
2021-06-13 19:53:36 +0900

    [encrypt-converter] ✨ encrypt-converter 구현

---
+++ encrypt-converter/src/test/java/com/example/encryptconverter/EncryptConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encryptconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptConverterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
